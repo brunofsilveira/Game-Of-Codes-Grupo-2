f96c8b8db6e411235a5ef483d1333da44817f862
Christopher Pottes <christopherpottesc@gmail.com>
2019-06-09 07:56:42 -0300

    Dados de locais cadastrados

--- resources/views/usuario/locais.blade.php
+++ resources/views/usuario/locais.blade.php
@@ -6,6 +6,13 @@
 .card .card-image img{
   height: 300px;
 }
+
+.card .card-content {
+    padding: 30px 10px;
+}
+div .card{
+  height: 490px;
+}
 </style>
 
 <div class="row">
@@ -14,11 +21,29 @@
       <div class="card">
         <div class="card-image" >
           <img src="{{ ("storage/img/garagem-1.jpg") }}">
-          <span class="card-title">Card Title</span>
           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
         <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          <div class="row">
+            <div class="col s6">
+              <p>Convêniado: João Marcos</p>
+            </div>
+            <div class="col s6">
+              <p style="text-align: right">Valor: R$0.25/h</p>
+            </div>
+
+            <div class="col s12">
+              <p>Endereço: Rua Gonçalves Chaves, 680, 95080-200</p>
+            </div>
+
+            <div class="col s6">
+              <p>Obs: Lugar coberto!</p>
+            </div>
+
+            <div class="col s6">
+              <p style="text-align: right"><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star</i></p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -28,11 +53,29 @@
         <div class="card">
           <div class="card-image">
             <img src="{{ ("storage/img/garagem-2.jpg") }}">
-            <span class="card-title">Card Title</span>
             <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
           </div>
           <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+              <div class="row">
+                  <div class="col s6">
+                    <p>Convêniado: Local Público</p>
+                  </div>
+                  <div class="col s6">
+                    <p style="text-align: right">Valor: R$0</p>
+                  </div>
+      
+                  <div class="col s12">
+                    <p>Endereço: Rua Félix da Cunha, 20, 95090-205</p>
+                  </div>
+      
+                  <div class="col s6">
+                    <p>Obs: </p>
+                  </div>
+      
+                  <div class="col s6">
+                    <p style="text-align: right"><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star</i></p>
+                  </div>
+                </div>
           </div>
         </div>
       </div>
@@ -42,11 +85,28 @@
         <div class="card">
           <div class="card-image">
             <img src="{{ ("storage/img/garagem-3.jpg") }}">
-            <span class="card-title">Card Title</span>
             <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
           </div>
           <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+              <div class="row">
+                  <div class="col s8">
+                    <p>Convêniado: Estacionamento Esquina</p>
+                  </div>
+                  <div class="col s4">
+                    <p style="text-align: right">Valor: 0.50R$/h</p>
+                  </div>
+      
+                  <div class="col s12">
+                    <p>Endereço: Rua General Osório, 150, 95060-200</p>
+                  </div>
+      
+                  <div class="col s8">
+                    <p>Obs: Mensalistas tem preço diferenciado!</p>
+                  </div>
+      
+                  <div class="col s4">
+                    <p style="text-align: right"><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star</i></p>
+                  </div>
           </div>
         </div>
       </div>
